e19370ffdbafab7d42819690d2aa1b10f75fd28c
Azaan Dinkins <addinkins@aggies.ncat.edu>
2026-06-07 08:49:58 -0400

    Added logic for type of temperature and equation to convert from F to C and vice versa

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class TemperatureConverter {
     public static double convertTemperature(double temperature, String unit) {
-        // TODO: students implement this
-        return 0.0;
+        double convertValue = 0.0;
+        if (unit.equals("C") || unit.equals("c")) {
+            convertValue = (temperature * 9.0/5.0) + 32.0;
+        }
+        else if (unit.equals("F") || unit.equals("f")) {
+            convertValue = (temperature - 32.0) * 5.0/9.0;
+        }
+        return convertValue;
     }
 
     public static void main(String[] args) {
-        // TODO: students implement this
+
     }
 
 }
