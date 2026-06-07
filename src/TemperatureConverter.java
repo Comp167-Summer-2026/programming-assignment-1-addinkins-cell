d04da10c5b58e70152f97d684aa999701b65adae
Azaan Dinkins <addinkins@aggies.ncat.edu>
2026-06-07 09:27:20 -0400

    Cleaned exception logic and added closeout

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -13,7 +13,42 @@ public class TemperatureConverter {
     }
 
     public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        boolean on = true;
+        while (on) {
+            System.out.print("Enter a temperature value or type 'stop' to quit: ");
+            String input = scnr.next();
+            if (input.equals("stop")) {
+                on = false;
+            }
+            else {
+                Scanner stringCheck = new Scanner(input);
+                if (stringCheck.hasNextDouble()) {
+                    double tempValue = stringCheck.nextDouble();
+                    System.out.print("Enter the unit C or F: ");
+                    String unit = scnr.next();
+                    if (unit.equals("C") || unit.equals("F")) {
+                        double result = convertTemperature(tempValue, unit);
+                        System.out.printf("%.2f°%s is equal to %.2f°%s\n",
+                                tempValue, unit.toUpperCase(),
+                                result, (unit.equals("C") ? "F" : "C"));
+                    }
+                    else {
+                        // Display error message for unrecognized unit labels
+                        System.out.println("Error, Unrecognized unit, Use C or F.");
+                    }
 
+                }
+                else {
+                    // Display error message for non-numeric temperature values (like "abc")
+                    System.out.println("Error: Invalid temperature input Please enter  valid number.");
+                }
+
+                stringCheck.close();
+            }
+        }
+
+        System.out.println("Program exited.");
+        scnr.close();
     }
-
 }
